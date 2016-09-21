615323318e865f3589ece35848663e96e2232a8a
AIT LAARAJ <layla.ait-laaraj@capgemini.com>
2016-09-21 17:38:18 +0100

    feat: add gulp tasks for jshint check

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+var gulp = require('gulp'),
+    shell = require('gulp-shell'),
+    jshint = require('gulp-jshint'),
+    stylish = require('jshint-stylish'),
+    runSequence = require('run-sequence');
+
+var options = require('minimist')(process.argv.slice(2));
+
+gulp.task('default', function() {
+    //run a sequence of tasks
+});
+
+gulp.task('jshint-client', function () {
+	return gulp.src(['./lib/*.js', './cap/ui_scripts/*.js', './src/business_rules/*.js'])
+		.pipe(jshint())
+		.pipe(jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('jshint-server', function () {
+	return gulp.src(['./lib/*.js','./cap/script_includes/*.js'])
+		.pipe(jshint())
+		.pipe(jshint.reporter(stylish));
+});
+
+gulp.task('jshint', function (callback) {
+	/**
+	 * This will run in this order: 
+	 * jshint-client 
+	 * jshint-client and jshint-server in parallel 
+	 * jshint-server 
+	 * Finally call the callback function
+	 */
+  	runSequence('jshint-client',
+				//['jshint-client', 'jshint-server'],
+				'jshint-server',
+				callback);
+})
